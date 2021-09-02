03a7a8641fa79e9316e0d2ed967475aa809cf6e7
adniasalsabila <adniasalsabila8@gmail.com>
2021-09-02 19:21:57 +0700

    Tugas2_GITS

---
+++ src/GanjilGenap.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var angka: Int
+    println("Menentukan Bilangan Ganjil dan Genap")
+    print("Masukkan angka = ")
+    angka = readLine()!!.toInt()
+    if (angka % 2 == 0) {
+        print("Angka $angka termasuk Bilangan Genap")
+    } else {
+        print("Angka $angka termasuk Bilangan Ganjilap")
+    }
+}
